223aee2bb4559871f290bbdf6910d9ce01797211
JBarboza20 <96068228+JBarboza20@users.noreply.github.com>
2021-12-15 19:11:34 -0500

    doPost()

--- my.js
+++ my.js
@@ -1,6 +1,6 @@
 function testGS(){
     
-    const url = "https://script.google.com/macros/s/AKfycbyx2ADQa3bqaxOMf8IcmRQw09gwUVX56zH55fG3XrPH86cCc0cDBf0-2wr1l-LmurK_/exec";
+    const url =  "https://script.google.com/macros/s/AKfycbzzEniFpO--XFyG6VRtbdZdxah9bQ_t_Go9T_G3ZVUtUXeA7yULJlH02AeTc3Ib-P1o/exec";
 
     fetch(url)
         .then(d => d.json())
@@ -13,3 +13,26 @@ function testGS(){
 }
 
 document.getElementById("btn").addEventListener("click", testGS);
+document.getElementById("btn2").addEventListener("click", addGS);
+
+
+// https://script.google.com/macros/s/AKfycbzzEniFpO--XFyG6VRtbdZdxah9bQ_t_Go9T_G3ZVUtUXeA7yULJlH02AeTc3Ib-P1o/exec
+
+function addGS(){
+    
+    const url =  "https://script.google.com/macros/s/AKfycbzzEniFpO--XFyG6VRtbdZdxah9bQ_t_Go9T_G3ZVUtUXeA7yULJlH02AeTc3Ib-P1o/exec";
+    fetch(url, {
+        method: 'POST',
+        mode: 'no-cors',
+        cache: 'no-cache',
+        credentials: 'omit',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        //referrerPolicy: 'no-referrer',
+        body: JSON.stringify({name:"Olivia"})
+    });
+        
+
+}
